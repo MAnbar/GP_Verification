0fafc8aa2ca02e9dbd2a79bc27396609e8d69370
Mohamed Anbar <M97.anbar@gmail.com>
2018-10-13 20:45:36 +0200

    Initial

--- src/gp/source/Main.java
+++ src/gp/source/Main.java
@@ -10,14 +10,12 @@ public class Main {
         System.loadLibrary( Core.NATIVE_LIBRARY_NAME );   
                 
         Verifier verifier = new Verifier();
-        Mat src = verifier.readImage("E1.jpg");
-
-        verifier.compare(src, src);
-//        verifier.detectAllImgLandmarks();
-        verifier.showImage(verifier.rotateScaleImg(src, 0,2));
-        verifier.showImage(src);
-
-        
-        
+        Mat src1 = verifier.readImage("E1.jpg");
+        Mat src2 = verifier.readImage("F2.jpg");
+        verifier.compare(src1, src2);
+        verifier.detectAllImgLandmarks();
+//        verifier.showImage(verifier.rotateScaleImg(src, 0,2));
+//        verifier.showImage(src);
+ 
     }
 }
--- src/gp/verification/Verifier.java
+++ src/gp/verification/Verifier.java
@@ -13,7 +13,6 @@ import org.opencv.face.Facemark;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import static org.opencv.imgproc.Imgproc.warpAffine;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class Verifier {
@@ -55,7 +54,9 @@ public class Verifier {
     public Mat readImage(String imgName){
         return Imgcodecs.imread(Input_Images_PATH+"/"+imgName);
     }
-    
+    public void writeImage(String imgName, Mat img){
+        Imgcodecs.imwrite(Output_Images_PATH+"/Out"+imgName+".jpg", img);
+    }
  //--============================================================================   
     public Mat rotateScaleImg(Mat src, double angle, double scale){
         Size size=new Size(src.cols(), src.rows());
@@ -63,7 +64,7 @@ public class Verifier {
         
         Point center = new Point(src.cols()/2, src.rows()/2);
         Mat r = Imgproc.getRotationMatrix2D(center, angle, scale);
-        warpAffine(src, dst, r, size);
+        Imgproc.warpAffine(src, dst, r, size);
         return dst;
     }
     
@@ -136,34 +137,74 @@ public class Verifier {
 //--============================================================================
     public Mat compare(Mat img1, Mat img2){
         
-        Mat sImage = img1.clone();
-        MatOfRect faces = new MatOfRect();
+        Mat sImage1;
+        Mat sImage2;
+        
+        MatOfRect faces1 = new MatOfRect();
+        MatOfRect faces2 = new MatOfRect();
 
-        classifierArray[0].detectMultiScale(sImage, faces);
-        if(faces.empty()){
+        classifierArray[0].detectMultiScale(img1, faces1);
+        classifierArray[0].detectMultiScale(img2, faces2);
+
+        if(faces1.empty()||faces2.empty()){
             return null;
         }
+        ArrayList<MatOfPoint2f> landmarks1= new ArrayList<>();
+        ArrayList<MatOfPoint2f> landmarks2= new ArrayList<>();
+        
+        fm1.fit(img1, faces1, landmarks1);
+        fm1.fit(img2, faces2, landmarks2);
 
-        ArrayList<MatOfPoint2f> landmarks= new ArrayList<>();
-        fm1.fit(sImage, faces, landmarks);
+        MatOfPoint2f lm1 = landmarks1.get(0);
+        MatOfPoint2f lm2 = landmarks2.get(0);
 
-        for(int z=0;z<landmarks.size();z++){
-            MatOfPoint2f lm = landmarks.get(z);
-
-            for(Integer h=0;h<lm.rows();h++){
-                double[] dp=lm.get(h,0);
-                Point p =new Point(dp[0], dp[1]);
-                Imgproc.circle(sImage, p, 2,new Scalar(0,255,255), 2);
-//              Imgproc.putText(origImg2, h.toString(), p, 1, 1, new Scalar(0,255,255));
-            }
-            System.out.println(getLandMarkPoint(lm, 27)+"-"+getLandMarkPoint(lm, 28));
+        
+        for(Integer h=0;h<lm1.rows();h++){
+            double[] dp=lm1.get(h,0);
+            Point p =new Point(dp[0], dp[1]);
+            Imgproc.circle(img1, p, 2,new Scalar(0,255,255), 2);
+            
+            dp=lm2.get(h,0);
+            p =new Point(dp[0], dp[1]);
+            Imgproc.circle(img2, p, 2,new Scalar(0,255,255), 2);
         }
-        return sImage;
+
+        Point left27i1=getLandMarkPoint(lm1, 27);
+        Point right28i1=getLandMarkPoint(lm1, 28);
+        
+        Point left27i2=getLandMarkPoint(lm2, 27);
+        Point right28i2=getLandMarkPoint(lm2, 28);
+
+        double dY1=right28i1.x-left27i1.x;
+        double dX1=right28i1.y-left27i1.y;
+        double angle1=Math.toDegrees(Math.atan2(dY1, dX1));
+        double length1= Math.sqrt(dX1*dX1+dY1*dY1);
+        Point eyes1Center = new Point( (left27i1.x + right28i1.x) * 0.5f, (left27i1.y + right28i1.y) * 0.5f );
+        System.out.println(angle1);
+        
+        double dY2=right28i2.x-left27i2.x;
+        double dX2=right28i2.y-left27i2.y;
+        double angle2=Math.toDegrees(Math.atan2(dY2, dX2));
+        double length2= Math.sqrt(dX1*dX1+dY1*dY1);
+        Point eyes2Center = new Point( (left27i2.x + right28i2.x) * 0.5f, (left27i2.y + right28i2.y) * 0.5f );
+        System.out.println(angle2);
+        
+        double scale=length2/length1;
+//        Mat rot_mat = Imgproc.getRotationMatrix2D(eyes1Center, angle1, scale);
+//        Imgproc.warpAffine(img1, sImage1, rot_mat, img2.size());                 
+//        Mat homography = Calib3d.findHomography(lm1, lm2);
+//        Imgproc.warpPerspective(img1, sImage1, homography, img2.size());
+        sImage1=rotateScaleImg(img1, -angle1, scale);
+        sImage2=rotateScaleImg(img2, -angle2, 1);
+        writeImage("SC1", sImage1);
+        writeImage("SC2", sImage2);
+        return sImage1;
     }
 
     public Point getLandMarkPoint(MatOfPoint2f lm, int PointID){
         double[] dp=lm.get(PointID,0);
         return new Point(dp[0], dp[1]);
     }
+
 }
 
